020073b34bde114b5e8274c98974d4a54adb5c8a
pawunder1992 <pavelkryvutsa@gmail.com>
2026-05-13 19:34:35 +0300

    update admin panel

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Worker, ServiceCategory, Client, Specialty, Order
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("specialty",)
+
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("specialty",)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "specialty",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ("license_plate", "owner_name")
+    list_display = ("license_plate", "model", "owner_name")
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "client", "category", "is_completed", "date")
+    list_filter = ("is_completed", "category", "date")
